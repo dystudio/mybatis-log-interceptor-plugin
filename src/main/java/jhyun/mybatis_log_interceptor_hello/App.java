9d0cd42405404638ceef0da3513f9b0db2d98cd3
jonghyouk yun <ageldama@gmail.com>
2013-11-09 21:00:45 +0900

    init.

---
+++ src/main/java/jhyun/mybatis_log_interceptor_hello/App.java
@@ -0,0 +1,13 @@
+package jhyun.mybatis_log_interceptor_hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
