f9204a0433e5b4235858638e34799e8baa5c92db
jonghyouk yun <ageldama@gmail.com>
2013-11-09 21:11:02 +0900

    test-case.

---
+++ src/test/java/jhyun/mlih/tests/FooTests.java
@@ -0,0 +1,31 @@
+package jhyun.mlih.tests;
+
+import jhyun.mlih.services.FooJdbcService;
+import jhyun.mlih.services.FooService;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class FooTests extends MyTestCase {
+
+	@Autowired
+	private FooJdbcService fooJdbcService;
+
+	@Autowired
+	private FooService fooService;
+
+	@Before
+	public void prepare() {
+		fooJdbcService.createSequence();
+		fooJdbcService.createTable();
+	}
+
+	@Test
+	public void createAndInsertAndCount() {
+
+	}
+
+	// TODO: bind-params?
+
+}
